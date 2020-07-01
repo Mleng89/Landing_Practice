63b14991647d09267814878c225cd1f8eb6c3de0
Matthew Leng <mleng89@gmail.com>
2020-07-01 18:55:51 -0400

    updated middle

--- src/Middle/Middle.js
+++ src/Middle/Middle.js
@@ -8,6 +8,22 @@ function Middle() {
 				<h2>Don't have a car? </h2>
 				<h3>Get a flexible rental with Lyft Express Drive</h3>
 			</section>
+			<div className="list-items">
+				<ul>
+					<li>Drive your way</li>
+					<p>Get a car any time—and return it when you want.</p>
+					<li>Cut Costs</li>
+					<p>Insurance and standard maintenance are always included.</p>
+					<li>Unlimited Lyft Miles</li>
+					<p>Drive unlimited miles for Lyft and get extra miles for personal use.</p>
+				</ul>
+			</div>
+			<p>
+				The more you drive, the less you pay with all inclusive rentals designed to help you earn on your terms.
+			</p>
+			<a target="_self" href="#">
+				Learn more
+			</a>
 		</div>
 	);
 }
